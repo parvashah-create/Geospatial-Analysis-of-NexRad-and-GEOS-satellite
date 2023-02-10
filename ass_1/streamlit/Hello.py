bc6b7425785100db234c14f41777676b96541f6c
parva shah <parvashah@parvas-MacBook-Pro.local>
2023-02-10 12:52:25 -0500

    minor changes

--- ass_1/streamlit/Hello.py
+++ ass_1/streamlit/Hello.py
@@ -10,25 +10,38 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.write("# Welcome to Streamlit! 👋")
+st.write("# Welcome! 👋")
 
 st.sidebar.success("Select a demo above.")
 
 st.markdown(
-    """ç
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    """
+    # Assignment 1
+    ## Team 14
+    - Parva Shah 
+    - Dev Shah
+    - Harsh Shah
+
+    This streamlit app has 3 pages:
+    - 1_🛰_Geos_Data_Downloader:
+
+        There are ywo methods to download a file:
+        - Download by Path:
+        User select the path and all files located on that path is displayed. The user can then select a file to download
+        - Download by Filename:
+        User writes the filename in the input box and a download link for the same is displayed.
+
+    - 2_📡_Nexrad_Data_Downloader
+    
+        There are ywo methods to download a file:
+        - Download by Path:
+        User select the path and all files located on that path is displayed. The user can then select a file to download
+        - Download by Filename:
+        User writes the filename in the input box and a download link for the same is displayed.
+
+    - 3_📍_NexRad_Locations
+        This plots the Nexrad locations on the US map
+
 """
 )
 
