859d4c2194aea5c52874cb80e7a2f1217b4d4fae
parva shah <parvashah@parvas-MacBook-Pro.local>
2023-02-10 19:09:25 -0500

    It will happen

--- streamlit/pages/3_📍_NexRad_Locations.py
+++ streamlit/pages/3_📍_NexRad_Locations.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 st.write("# Nexrad Locations in USA 📍")
-df = pd.read_csv('./nexrad_loc.csv')
+df = pd.read_csv('streamlit/nexrad_loc.csv')
 df['text'] = 'City: ' + df['City'] + ', ' + 'State: '+ df["State"] + ', ' + 'Identifier: ' + df['ICAO Location Identifier'].astype(str)
 
 
