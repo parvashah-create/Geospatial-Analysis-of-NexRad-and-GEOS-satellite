90daee19a87b5f021d4a8b596927aa8676c0e2de
Parva Jigar Shah <69401223+parvashah-create@users.noreply.github.com>
2023-02-24 19:19:13 -0500

    Update 1_🛰_Geos_Data_Downloader.py

--- streamlit/pages/1_🛰_Geos_Data_Downloader.py
+++ streamlit/pages/1_🛰_Geos_Data_Downloader.py
@@ -38,7 +38,7 @@ def upload_file_to_s3(file_name, prefix, source_bucket_name, target_bucket_name)
     # Upload the content of the file to the target object
     target_object.upload_fileobj(io.BytesIO(file_content))
     # Print a message indicating that the file has been uploaded
-    check = "File {file_name} with prefix {prefix} uploaded to S3 bucket {target_bucket_name}."
+    check = f"File {file_name} with prefix {prefix} uploaded to S3 bucket {target_bucket_name}."
 
     # Return link to uploaded file
     # Construct the URL of the uploaded file
